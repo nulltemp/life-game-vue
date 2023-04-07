fbaf64ea795e04b28bc6327bfc24678f3523ab9a
nulltemp <nulltemp999@gmail.com>
2023-04-07 23:15:26 +0900

    サイズが可変になるように修正

--- src/components/LifeGame.vue
+++ src/components/LifeGame.vue
@@ -3,13 +3,16 @@
     <div class="map">
       <template v-for="(dataList, i) in dataMap" :key="i">
         <template v-for="(data, j) in dataList" :key="j">
-          <div class="box" @click="changeDataMapValue(i, j)">
-            {{ data }}
+          <div class="box" @click="changeDataMapValue(i, j)" :class="data === 1 ? 'live' : 'death'">
           </div>
         </template>
       </template>
-      <v-btn @click="changeStartStatus">{{ startButtonMessage }}</v-btn>
     </div>
+    <div>
+      <v-text-field input v-model="height" @focus="stopStatus()" label="height" @change="changeDataMapSize()"/>
+      <v-text-field input v-model="width" @focus="stopStatus()" label="width" @change="changeDataMapSize()"/>
+    </div>
+    <v-btn @click="changeStartStatus">{{ startButtonMessage }}</v-btn>
   </div>
 </template>
 
@@ -29,8 +32,41 @@ const dataMap = ref([
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ])
 
+const width = ref(10)
+const height = ref(10)
+
 const changeDataMapValue = (i: number, j: number) => {
   dataMap.value[i][j] = dataMap.value[i][j] === 1 ? 0 : 1
+}
+
+const changeDataMapSize = () => {
+  if (width.value !== dataMap.value[0].length) {
+    for (let i = 0; i < dataMap.value.length; i++) {
+      if (width.value < dataMap.value[i].length) {
+        dataMap.value[i].length = width.value
+      } else {
+        const prevWidth = dataMap.value[i].length
+        dataMap.value[i].length = width.value
+        for (let j = prevWidth; j < width.value; j++) {
+          dataMap.value[i][j] = 0
+        }
+      }
+    }
+  }
+  if (height.value !== dataMap.value.length) {
+    if (height.value < dataMap.value.length) {
+      dataMap.value.length = height.value
+    } else {
+      const prevHeight = dataMap.value.length
+      dataMap.value.length = height.value
+      for (let i = prevHeight; i < height.value; i++) {
+        dataMap.value[i] = []
+        for (let j = 0; j < width.value; j++) {
+          dataMap.value[i][j] = 0
+        }
+      }
+    }
+  }
 }
 
 const isStarted = ref(false)
@@ -98,15 +134,31 @@ const changeStartStatus = () => {
     update()
   }
 }
+
+const stopStatus = () => {
+  if (isStarted.value) {
+    isStarted.value = false
+    startButtonMessage.value = "start"
+  }
+}
 </script>
 
 <style scoped>
 .map {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: repeat(v-bind(width), 1fr);
 }
 
 .box {
   border: 1px solid #000;
+  height: 1em;
+}
+
+.live {
+  background: #000;
+}
+
+.death {
+  background: #FFF;
 }
 </style>
